8f7a9ccde27fda605f3c3c591c06b566cc1d4104
Serge Vakulenko <serge.vakulenko@gmail.com>
2020-08-27 00:06:07 -0700

    Add tests. Wildcards for output comparison not implemented yet, so some tests fail on time/date sensitive output.

--- README.md
+++ README.md
@@ -19,3 +19,39 @@ The simulator consists of three components:
     cmake ..
     make
     make install
+
+# Testing
+To run all available tests:
+
+    cd build
+    make test
+
+Expected output:
+
+    $ make test
+    Running tests...
+    Test project /Users/vak/Project/Besm-6/dispak/build
+        Start 1: algol-besm6
+    1/3 Test #1: algol-besm6 ......................   Passed    0.05 sec
+        Start 2: algol-gdr
+    2/3 Test #2: algol-gdr ........................   Passed    0.06 sec
+        Start 3: fortran-dubna
+    3/3 Test #3: fortran-dubna ....................   Passed    0.09 sec
+
+    100% tests passed, 0 tests failed out of 3
+
+    Total Test time (real) =   0.20 sec
+
+To see, why a particular test fails, use:
+
+    cd build
+    ctest -V -R test-name
+
+To create a new test:
+  * Create a directory for it, say tests/mytest
+  * Put here a file with input for dispak, for example 'mytest.b6'.
+  * Create a file tests/mytest/options.txt with command line parameters for dispak. The file should contains at least a filename of the input file, i.e. mytest.b6.
+  * Create a file with expected output 'output.txt'. It can be obtained by manually running dispak and redirecting the output to a file. For example: "dispak mytest.b6 > output.txt".
+  * Add a line to the file tests/CMakeLists.txt for your test. Like this:
+
+    add_test(NAME mytest COMMAND Python3::Interpreter ../run-test.py WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests/mytest)
---
+++ tests/run-test.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+#
+# Run a test in current directory.
+#
+import sys, os
+
+#--------------------------------------------------------------
+# Parse the command line arguments:
+#   arg.verbosity  - level of verbosity
+#   arg.help       - usage info requested
+#
+import argparse
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("-v", "--verbosity", action="count", default=0)
+parser.add_argument("-h", "--help", action="count", default=0)
+arg = parser.parse_args()
+if arg.help:
+    print("Usage:")
+    print("    run-test.py [options]")
+    print("");
+    print("Run test in current directory.");
+    print("");
+    print("Options:");
+    print("    -v, --verbosity    Increase verbosity")
+    print("    -h, --help         Show this message and exit")
+    sys.exit(0)
+
+#--------------------------------------------------------------
+# Find application binary.
+#
+def get_app_path(app_name, relative_path):
+
+    # Find application directory based on a path of this script.
+    if getattr(sys, 'frozen', False):
+        path = os.path.dirname(sys.executable)
+    else:
+        try:
+            path = os.path.dirname(os.path.realpath(__file__))
+        except NameError:
+            return app_name
+
+    # Try relative path.
+    path = os.path.normpath(path + "/" + relative_path)
+    if os.path.isfile(path):
+        return path
+
+    # No executable in the build directory: search the PATH.
+    return app_name
+
+#--------------------------------------------------------------
+# Get options from file.
+#
+def get_options(file_name):
+    if not os.path.isfile(file_name):
+        print(f"FATAL: Cannot find {file_name} in current directory")
+        sys.exit(-1)
+    with open(file_name, 'r') as file:
+        return ' '.join(file.readlines()).split()
+
+#--------------------------------------------------------------
+# Run application with given command line parameters.
+#
+def run_test(app, options, output_name):
+    if arg.verbosity:
+        print("Run:", app, options)
+
+    import subprocess
+    with open(output_name, 'w') as file:
+        status = subprocess.call([app] + options, shell=False, stdout=file, stderr=subprocess.STDOUT)
+
+    if arg.verbosity:
+        print("Status:", status)
+
+    if status != 0:
+        # The program terminated with error status.
+        print(f"FATAL: {app} failed with status {status}")
+        sys.exit(-1)
+
+#--------------------------------------------------------------
+# Compare the output with expected contents.
+#
+def compare_output(output_name, expected_name):
+    if not os.path.isfile(expected_name):
+        print(f"FATAL: Cannot find {expected_name} in current directory")
+        print(f"FATAL: Please rename {output_name} to {expected_name}")
+        sys.exit(-1)
+
+    # Read file contents.
+    with open(output_name, 'r') as file:
+        output = file.readlines()
+    with open(expected_name, 'r') as file:
+        expected = file.readlines()
+
+    import difflib
+    return difflib.unified_diff(expected, output)
+
+#--------------------------------------------------------------
+# Compare the output with expected contents.
+#
+def process_wildcards(diff):
+    # TODO: match wildcards
+    return list(diff)
+
+#--------------------------------------------------------------
+#
+application_path = get_app_path("dispak", "../build/dispak/dispak")
+if arg.verbosity:
+    print("Application:", application_path)
+
+options = get_options("options.txt")
+if arg.verbosity:
+    print("Options:", options)
+
+run_test(application_path, options, "output.run")
+
+diff = compare_output("output.run", "output.txt")
+
+diff = process_wildcards(diff)
+
+if len(diff) > 0:
+    sys.stdout.writelines(diff)
+    print("FATAL: Output does not match the sample")
+    sys.exit(-1)
